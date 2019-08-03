1cc80d51d1d6cfc63e2c233d14ac596486d7f14e
bombin78 <bombin78@mail.ru>
2019-08-03 23:40:03 +0300

    Переход на gulp 4 + настройка синхронизации на изменения scss, js, json и php файлов + добавлена минификация js и css фалов

--- gulpfile.js
+++ gulpfile.js
@@ -1,78 +1,81 @@
-'use strict';
-
-var gulp = require('gulp'),
+const gulp = require('gulp'),
     sass = require('gulp-sass'),
-    uglify = require('gulp-uglify'),
+    cleanCSS = require('gulp-clean-css'),
     sourcemaps = require('gulp-sourcemaps'),
     autoprefixer = require('gulp-autoprefixer'),
-    cache = require('gulp-cache'),
-    imagemin = require('gulp-imagemin'),
-    pngquant = require('imagemin-pngquant'),
-    concat = require('gulp-concat');//,
-    // vendorsJsFiles = [
-    //     './src/vendor/jquery/jquery.min.js',
-    //     './src/vendor/angular/angular-1.6.4.min.js',
-    //     './src/vendor/angular/angular-sanitize.js',
-    //     './src/vendor/inputmask/jquery.inputmask.bundle.min.js',
-    //     './src/vendor/fancybox/js/jquery.fancybox.min.js',
-    //     './src/vendor/ng-file-upload/ng-file-upload-all.min.js',
-    //     './src/vendor/changeNumbers/changeNumbers.js',
-    //     './src/vendor/swiper/js/swiper.min.js',
-    //     './src/vendor/ui-select/select.js'
-    //];
-var cssimport = require('gulp-cssimport');
-//
-// gulp.task('import', function() {
-//     gulp.src('./dist/style.css')
-//         .pipe(cssimport())
-//         .pipe(gulp.dest('./dist'));
-// });
+    cssimport = require('gulp-cssimport'),
+    uglify = require('gulp-uglify'),
+    del = require('del'),
+    connect = require('gulp-connect-php'),
+    browserSync = require('browser-sync').create();
 
-// gulp.task('vendorJs', function() {
-//     return gulp.src(vendorsJsFiles)
-//         .pipe(sourcemaps.init())
-//         .pipe(concat('vendor.js'))
-//         .pipe(uglify())
-//         .pipe(sourcemaps.write('.'))
-//         .pipe(gulp.dest('./dist'));
-// });
 
-gulp.task('sass', function () {
-    return gulp.src('./src/scss/style.scss')
+const paths = {
+    scss: ['./src/scss/style.scss'],
+    jsFiles: [
+        './src/js/ie6-7blocker.js',
+        './src/js/modules-control.js'
+    ]
+};
+
+function styles() {
+    return gulp.src(paths.scss)
         .pipe(sourcemaps.init())
-        .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+        //
+        .pipe(sass({
+            outputStyle: 'compressed'
+        }).on('error', sass.logError))
+        //
         .pipe(cssimport())
+        // Добавить префиксы (версии браузеров смотри в файле "package.json")
         .pipe(autoprefixer())
-        .pipe(sourcemaps.write('.'))
-        .pipe(gulp.dest('./css'));
-});
+        // Минификация CSS
+        .pipe(cleanCSS({
+            level: 2
+        }))
+        .pipe(sourcemaps.write('./'))
+        // Выходная папка стилей
+        .pipe(gulp.dest('./css'))
+        // Обновление страницы
+        .pipe(browserSync.stream());
+}
 
-// gulp.task('jsMin', function () {
-//    return gulp.src('./src/js/main.js')
-//        .pipe(sourcemaps.init())
-//        .pipe(uglify({
-//            compress: {drop_debugger: false}
-//        }))
-//        .pipe(sourcemaps.write('.'))
-//        .pipe(gulp.dest('./dist'));
-// });
+function scripts() {
+    return gulp.src(paths.jsFiles)
+        .pipe(sourcemaps.init())
+        // Минификация JS
+        .pipe(uglify())
+        .pipe(sourcemaps.write('./'))
+        .pipe(gulp.dest('./js'))
+        // Обновление страницы
+        .pipe(browserSync.stream());
+}
 
-// gulp.task('serve', ['sass', 'vendorJs'], function () {
-//     gulp.watch('src/**/*.scss', ['sass']);
-//     gulp.watch(vendorsJsFiles, ['vendorJs']);
-//     gulp.watch('src/js/main.js', ['jsMin']);
-// });
+function clean() {
+    return del(['./css/*', './js/*']);
+}
 
-// gulp.task('imgMin', function () {
-//     gulp.src('dist/img/**/*', {base: './'})
-//         .pipe(cache(imagemin({
-//             interlaced: true,
-//             progressive: true,
-//             svgoPlugins: [{removeViewBox: false}],
-//             use: [pngquant()]
-//         })))
-//         .pipe(gulp.dest('./'))
-// });
+function watcher() {
 
-//gulp.task('default', ['sass', 'jsMin', 'serve']);
-gulp.task('default', ['sass']);
+    connect.server({}, function() {
+        browserSync.init({
+            proxy: '127.0.0.1:4000'
+        });
+    });
+
+    // Следит за изменениями scss файлов в папке "src/scss"
+    gulp.watch('src/scss/**/*.scss', styles);
+    // Следит за изменениями js файлов  в папке "src/js"
+    gulp.watch('src/js/**/*.js', scripts);
+    // При изменении json файлов запустить синхронизацию
+    //gulp.watch('tpl/**/*.json').on('change', browserSync.reload);
+    // При изменении php или json файлов запустить синхронизацию
+    gulp.watch(['**/*.php', 'tpl/**/*.json']).on('change', browserSync.reload);
+}
+
+// Task для отслеживания изменений
+gulp.task('watcher', watcher);
+// Task для удлания файлов в папках "css" и "js" и запуска "styles" и "scripts"
+gulp.task('build', gulp.series(clean, gulp.parallel(styles, scripts)));
+// Task для удаления старой сборки, запуска новой сборки и "watcher"a
+gulp.task('dev', gulp.parallel('build', 'watcher'));

45bc5c17e19da7d552737f4418af58bd699e5d6d
bombin78 <bombin78@mail.ru>
2019-08-31 11:05:16 +0300

    Поправлены ковычки

--- tpl/blocks/header.php
+++ tpl/blocks/header.php
@@ -1,7 +1,7 @@
 <header class="header">
 	<div class="header__block">
 		<div class="header__block-control control-panel">
-			<a class="control-panel__logo" href='<?php echo $path; ?>'><b><?php echo $nameCompany; ?></b></a>
+			<a class="control-panel__logo" href="<?php echo $path; ?>"><b><?php echo $nameCompany; ?></b></a>
 			<!-- главное меню -->
 			<?php include_once $path."tpl/blocks/menu/menu.php" ?>
 			<!-- /главное меню -->

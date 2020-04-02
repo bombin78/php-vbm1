0e0e22588b6b47e0005bc37e79fc7855e9008041
bombin78 <bombin78@mail.ru>
2020-04-02 23:58:43 +0300

    Редактирование шаблона

--- tpl/blocks/footer.php
+++ tpl/blocks/footer.php
@@ -1,15 +1,23 @@
 <footer class="footer">
-	<div class="footer__company">
-		<?php if($copyright) echo $copyright; ?>&nbsp;&nbsp;
-		<?php if($nameCompany) echo $nameCompany; ?>
-		<br>
-		e-mail: <?php if($eMail) echo $eMail; ?>
+
+	<div class="footer-company">
+		<?= $copyright?>&nbsp;&nbsp;
+		<?= $nameCompany?>
+		<dl class="footer-company__dtl dtl">
+			<dt class="dtl__l">E-mail:</dt>
+			<dd class="dtl__v"><?= $eMail?></dd>
+		</dl>
 	</div>
-	<div class="footer__location">
-		<p><?php if($location) echo $location; ?></p>
+
+	<div class="footer-location">
+		<p><?= $location?></p>
 	</div>
-	<div class="footer__phones">
-		<?php if($numPhone_1) echo $numPhone_1; ?><br>
-		<?php if($numPhone_2) echo $numPhone_2; ?>
+
+	<div class="footer-phones">
+		<dl class="footer-phones__dtl dtl">
+			<dt class="dtl__l">Тел:</dt>
+			<dd class="dtl__v"><?= $numPhone?></dd>
+		</dl>
 	</div>
+	
 </footer>
